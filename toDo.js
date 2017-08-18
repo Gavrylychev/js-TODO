6881eb6ed7b18debb05d7f1ce791ec9a27ddbd78
shamanbronya <gavrylychevalex@gmail.com>
2017-08-18 12:46:00 +0300

    upgrade

--- toDo.js
+++ toDo.js
@@ -1,5 +1,3 @@
-var ulElem = document.createElement("ul");
-document.body.appendChild(ulElem);
 var inputElem = document.createElement("input");
 inputElem.setAttribute("type", "text");
 document.body.appendChild(inputElem);
@@ -7,8 +5,21 @@ var addButton = document.createElement("button");
 addButton.setAttribute("type", "button");
 addButton.innerText = "Add text";
 document.body.appendChild(addButton);
+var ulElem = document.createElement("ul");
+document.body.appendChild(ulElem);
 var inputTextValue = "";
 
+function giveColorLi(){
+  var li = document.querySelectorAll("li");
+  for(var i = 0; i < li.length; i++){
+    li[i].addEventListener("click", function(){
+      alert("click");
+      // this.classList.toggle("coloredLi");
+
+    })
+  }
+}
+giveColorLi();
 function createLi(arrStr){
   for(var i = 0; i < arrStr.length; i++){
     var liElem = document.createElement("li");
@@ -18,30 +29,24 @@ function createLi(arrStr){
 }
 createLi(["first", "second", "third"]);
 
-function valueOfInput(){
-  inputElem.addEventListener("keypress", function(event){
-    inputTextValue = event.target.value;
-      if(event.keyCode === 13){
-        var liElem = document.createElement("li");
-        liElem.innerHTML = inputTextValue;
-        ulElem.appendChild(liElem);
-        this.value = "";
-      }
-  });
-}
-valueOfInput();
+inputElem.addEventListener("keypress", function(event){
+  inputTextValue = event.target.value;
+    if(event.keyCode === 13){
+      var liElem = document.createElement("li");
+      liElem.innerHTML = inputTextValue;
+      ulElem.appendChild(liElem);
+      this.value = "";
+    }
+});
 
-function addTextToLi(){
-  addButton.addEventListener("click", function(){
-    inputTextValue = inputElem.value;
-      if(inputElem.value === ""){
-        alert("add some text in input");
-      }else{
-        var liElem = document.createElement("li");
-        liElem.innerHTML = inputTextValue;
-        ulElem.appendChild(liElem);
-        inputElem.value = "";
-      }
-  });
-}
-addTextToLi();
+addButton.addEventListener("click", function(){
+  inputTextValue = inputElem.value;
+    if(inputTextValue === ""){
+      alert("add some text in input");
+    }else{
+      var liElem = document.createElement("li");
+      liElem.innerHTML = inputTextValue;
+      ulElem.appendChild(liElem);
+      inputElem.value = "";
+    }
+});
